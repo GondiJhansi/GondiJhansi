d7ab0f28303862fa37349551d63a9c538a7bcd6e
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-08-24 22:59:27 +0530

    Add files via upload

---
+++ While2.java
@@ -0,0 +1,15 @@
+package jhansi;
+
+public class While2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Printing 1 to 10 numbers");
+		int i = 1 ;
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+}
