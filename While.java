c3d3b2adad01a3c7f75acbfd4db6afc807deae1a
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-08-24 22:59:12 +0530

    Add files via upload

---
+++ While.java
@@ -0,0 +1,19 @@
+package jhansi;
+
+import java.util.Scanner;
+
+public class While
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Printing WELCOME Message");
+		int i = 1 ;
+		while(i<=5)
+		{
+			System.out.println("WELCOME");
+			i++;
+		}
+	}
+}
+
+		
