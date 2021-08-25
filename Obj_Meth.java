09e5b02ad354e4451cb140b7872ead299ec74f07
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-08-25 22:34:54 +0530

    Add files via upload

---
+++ Obj_Meth.java
@@ -0,0 +1,29 @@
+package java;
+
+public class Obj_Meth 
+{
+	public void m1()
+	{
+		System.out.println("It is m1 method");
+	}
+	public void m2()
+	{
+		System.out.println("It is m2 method");
+	}
+	public void m3()
+	{
+		System.out.println("It is m3 mthod");
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		Obj_Meth obj = new Obj_Meth();
+		obj.m1();
+		obj.m2();
+		obj.m3();
+		
+
+	}
+
+}
