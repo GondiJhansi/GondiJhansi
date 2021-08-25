a45c2ee9301bcbf2dba4c0ed2b41309fc5828248
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-08-25 22:35:16 +0530

    Add files via upload

---
+++ Obj1.java
@@ -0,0 +1,25 @@
+
+
+class Obj1
+{
+	public void m1()
+	{
+		System.out.println("It is m1 method");
+	}
+	public void m2()
+	{
+		System.out.println("It is m2 method");
+	}
+	public void m3()
+	{
+		System.out.println("It is m3 mthod");
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Hello");
+		Obj o = new Obj();
+		o.m1();
+	}
+}
