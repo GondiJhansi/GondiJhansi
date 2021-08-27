61e8f06ab53f7a40c23d516957a1f92264367666
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-08-27 19:54:05 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,14 @@
+package File;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=10,y=20,z;
+		z=x+y;
+		System.out.println("Sum ="+z);
+		
+	}
+
+}
