90c594efcfc68dc47aad3e8130d48500914d0c2c
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-08-27 19:55:51 +0530

    Add files via upload

---
+++ TypeConversion.java
@@ -0,0 +1,14 @@
+package File;
+
+public class TypeConversion
+{
+
+	public static void main(String[] args)
+	{
+		int x=30;
+		float y ;
+		y=x;  // y==30.000000.
+		System.out.println(y);
+	}
+
+}
