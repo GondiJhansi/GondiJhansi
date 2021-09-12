6f474c3dcff4c3e0c762492a26401d8662a3c627
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-09-12 22:50:53 +0530

    Add files via upload

--- Sample.java
+++ Sample.java
@@ -1,14 +1,26 @@
-package File;
+package Exceptions;
+class ExceptionA extends Exception
+{
+	public ExceptionA(String msg)
+	{
+		super(msg);
+	}
+}
 
-public class Sample {
+public class Sample 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		int x=10,y=20,z;
-		z=x+y;
-		System.out.println("Sum ="+z);
-		
+	public static void main(String[] args) 
+	{
+		try
+		{
+			System.out.println("Welcome");
+			throw new Exception("Hello ECE");
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
 	}
 
 }
