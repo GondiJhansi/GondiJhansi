8242e2750b072818c07f9f1f33343f26e59e0511
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-09-18 07:11:52 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,26 @@
+package File;
+
+public class DataTypes
+{
+
+	public static void main(String[] args) 
+	{
+		byte b = 125;
+		short s = -100;
+		int i = 42500;
+		long l = 423344678l;
+		float f = 3.14f;
+		double d = 42.33;
+		boolean bb = true;
+		char ch = 'J';
+		System.out.println("Byte = " +b);
+		System.out.println("Short = " +s);
+		System.out.println("Int = " +i);
+		System.out.println("Long = " +l);
+		System.out.println("Float = " +f);
+		System.out.println("Double = " +d);
+		System.out.println("Boolean = " +bb);
+		System.out.println("Char = " +ch);
+	}
+
+}
